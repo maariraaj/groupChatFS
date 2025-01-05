9c4895b7469df712ba03110816c76e90a0dcc6f1
Mariraj <maari000@hotmail.com>
2025-01-05 14:59:12 +0530

    for render deploy

--- util/database.js
+++ util/database.js
@@ -1,9 +1,28 @@
 const { Sequelize } = require("sequelize");
-require('dotenv').config();
+require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DB_SCHEMA, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-});
+let sequelize;
+
+if (process.env.NODE_ENV === "production") {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: "postgres",
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
+    });
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_SCHEMA,
+        process.env.DB_USERNAME,
+        process.env.DB_PASSWORD,
+        {
+            host: process.env.DB_HOST,
+            dialect: process.env.DB_DIALECT,
+        }
+    );
+}
 
 module.exports = sequelize;
